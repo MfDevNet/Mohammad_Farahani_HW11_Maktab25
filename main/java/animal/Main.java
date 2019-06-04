c94146f331cd159fc0d82bb0f5d4671af8294093
MfDevNet <MfDevNet@gmail.com>
2019-06-04 05:50:24 +0430

    Stream Collect With StartWith breed with "A" , "P" , "C"

--- main/java/animal/Main.java
+++ main/java/animal/Main.java
@@ -4,25 +4,37 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Cat cat=new Cat();
+        Cat cat = new Cat();
 //        cat.setName("ani");
         cat.setBreed(BreedType.Aegean);
 //        System.out.println(cat);
-        List<Animal> list=new ArrayList<Animal>();
+        List<Cat> list = new ArrayList<>();
         List<BreedType> arr = Arrays.asList(BreedType.values());
 
-        for (int i=0;i<99;i++){
-            cat=new Cat();
-            cat.setName("cat"+i);
+        for (int i = 0; i < 99; i++) {
+            cat = new Cat();
+            cat.setName("cat" + i);
             cat.setBreed(arr.get(i));
             list.add(cat);
         }
 
 
-        System.out.println(list);
+        Stream<Cat> l = list.stream().filter(e -> e.getBreed().name().startsWith("A") || e.getBreed()
+                .name().startsWith("P") || e.getBreed()
+                .name().startsWith("C"));
+
+        List<Cat> listCollectCharAPC = l.collect(Collectors.toList());
+        System.out.println(listCollectCharAPC);
+
+
+
+
+//        System.out.println(list);
 
 
     }
